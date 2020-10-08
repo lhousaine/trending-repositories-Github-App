1c0016b117cd3fc5d99f85eb7c2bb9776c09a143
lhousaine <l.ouarhou.ensa@gmail.com>
2020-10-08 22:58:21 +0100

    add public infos endpoint
    As I am using the private/public key, other services need to get the public key to decrypt the token. Also I am planing to integrate istio service meeh with my microservices architecture, So I add an endpoint that will return the JWTSet as json object

---
+++ src/Auth-service/src/main/java/com/ouarhou/authservice/presentation/resources/JwkSetEndpoint.java
@@ -0,0 +1,52 @@
+package com.ouarhou.authservice.presentation.resources;
+
+import java.security.KeyPair;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Base64;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.ouarhou.authservice.core.security.RsaKeyGeneration;
+
+@RestController
+@RequestMapping(path = "/keyInfos")
+public class JwkSetEndpoint {
+    private RSAKey jwk;
+    @Inject
+    public JwkSetEndpoint() {
+        RsaKeyGeneration rsaKeyGeneration=RsaKeyGeneration.getInstance();
+        this.jwk = rsaKeyGeneration.getRsaKey();
+    }
+
+    @GetMapping("/.well-known/jwks.json")
+    @ResponseBody
+    public Map<String, Object> getKey() {
+        return new JWKSet(jwk).toJSONObject();
+    }
+
+    @GetMapping("/public/key")
+    @ResponseBody
+    public String getPublicKey() throws JOSEException {
+        return convertToPublicKey(jwk.toRSAPublicKey());
+    }
+
+    private String convertToPublicKey(RSAPublicKey publicKey){
+        String key=Base64.getEncoder().encodeToString(publicKey.getEncoded());
+        StringBuilder result = new StringBuilder();
+        result.append("-----BEGIN PUBLIC KEY-----\n");
+        result.append(key);
+        result.append("\n-----END PUBLIC KEY-----");
+        return result.toString();
+    }
+}
+
