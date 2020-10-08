77658f55f62b7606d7714afe6b56a1b29a163557
lhousaine <l.ouarhou.ensa@gmail.com>
2020-10-08 23:10:34 +0100

    add Setup Database Initializer
    to test functionnality of the application initialize the Db WITH Two User Account

--- src/Auth-service/src/main/java/com/ouarhou/authservice/AuthServiceApplication.java
+++ src/Auth-service/src/main/java/com/ouarhou/authservice/AuthServiceApplication.java
@@ -1,7 +1,11 @@
 package com.ouarhou.authservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.ouarhou.authservice.core.setup.Initializer;
 
 @SpringBootApplication
 public class AuthServiceApplication {
@@ -9,4 +13,10 @@ public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
+
+    @Bean
+    public CommandLineRunner setupDB(){
+        return args -> Initializer.initializeDB();
+    }
+    
 }
---
+++ src/Auth-service/src/main/java/com/ouarhou/authservice/core/setup/Initializer.java
@@ -0,0 +1,37 @@
+package com.ouarhou.authservice.core.setup;
+
+import javax.inject.Inject;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.ouarhou.authservice.data.entities.UserAccount;
+import com.ouarhou.authservice.domain.dtos.UserAccountRequestDTO;
+import com.ouarhou.authservice.domain.usecases.iusecases.CreateUserAccountUseCase;
+
+public class Initializer {
+
+    private static CreateUserAccountUseCase createUserAccountUseCase;
+
+    @Inject
+    public Initializer(CreateUserAccountUseCase createUserAccountUseCase) {
+        Initializer.createUserAccountUseCase=createUserAccountUseCase;
+    }
+
+    public static void initializeDB(){
+        UserAccount userAccount1 =UserAccount.builder()
+            .firstName("ouarhou")
+            .lastName("ouarhou")
+            .username("ouarhou")
+            .password("ouarhou")
+            .build();
+
+        UserAccount userAccount2 =UserAccount.builder()
+            .firstName("lhou")
+            .lastName("lhou")
+            .username("lhou")
+            .password("lhou")
+            .build();
+        createUserAccountUseCase.execute(new UserAccountRequestDTO(userAccount1,userAccount1.getPassword()));
+        createUserAccountUseCase.execute(new UserAccountRequestDTO(userAccount2,userAccount2.getPassword()));
+    }
+}
