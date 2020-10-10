c83db0016b4a998effc16048f615b5bc1a9161ec
lhousaine <l.ouarhou.ensa@gmail.com>
2020-10-10 23:03:30 +0100

    create Frontend Models
    Before dealing with services and state management, firstly I create the Models in the Data Module, that contains UserAccount for dealing with User Sign In/Up,and Language for tranding repositories languages anad the last and p^rincipale one is the repository Model

---
+++ src/frontend/src/app/data/models/Repository.ts
@@ -0,0 +1,9 @@
+import { Interface } from "readline";
+
+export interface Repository{
+    id: number;
+    name: string;
+    ownerLogin: string;
+    language: string;
+    starsNumber: number;
+}
---
+++ src/frontend/src/app/data/models/UserAccount.ts
@@ -0,0 +1,7 @@
+export interface UserAccount {
+    id: number;
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+}
