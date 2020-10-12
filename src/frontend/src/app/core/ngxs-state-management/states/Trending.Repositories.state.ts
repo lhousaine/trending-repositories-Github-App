9055577ba1fe823cbdcb2a768b9206e3ecf254cb
lhousaine <l.ouarhou.ensa@gmail.com>
2020-10-12 23:18:23 +0100

    fix loading old Language repositories
    When I click a language to get it's repositories I get those of the last Oldest One in the state, so fix I add a subscriber to the action of setting new Language if it return success I redirect to the repositories page

--- src/frontend/src/app/core/ngxs-state-management/states/Trending.Repositories.state.ts
+++ src/frontend/src/app/core/ngxs-state-management/states/Trending.Repositories.state.ts
@@ -2,69 +2,83 @@ import { Action, Selector, State, StateContext } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { Repository } from 'src/app/data/models/Repository';
 import { RepositoryService } from 'src/app/data/services/repository.service';
-import { GetTrendingRepositoriesLanguages, SetCurrentLanguage } from '../actions/Trending.Repositories.actions';
-import {tap} from 'rxjs/operators';
+import {
+  GetTrendingRepositoriesLanguages,
+  SetCurrentLanguage,
+} from '../actions/Trending.Repositories.actions';
+import { tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 
 export class TrendingRepositoryStateModel {
-    trendingRepositoriesLanguages: string[];
-    currentLanguage?: string;
-    languageTrendingRepositories: Repository[];
+  trendingRepositoriesLanguages: string[];
+  currentLanguage?: string;
+  languageTrendingRepositories: Repository[];
 }
 const defaults: TrendingRepositoryStateModel = {
-    trendingRepositoriesLanguages: [],
-    currentLanguage: null,
-    languageTrendingRepositories: []
+  trendingRepositoriesLanguages: [],
+  currentLanguage: null,
+  languageTrendingRepositories: [],
 };
 
 @State<TrendingRepositoryStateModel>({
-    name: 'repositories',
-    defaults
+  name: 'repositories',
+  defaults,
 })
 @Injectable()
 export class TrendingRepositoryState {
+  constructor(private repositoryService: RepositoryService) {}
 
-    constructor(private repositoryService: RepositoryService) {
-    }
+  @Selector()
+  static getTrendingRepositoriesLanguages(
+    state: TrendingRepositoryStateModel
+  ): string[] {
+    return state.trendingRepositoriesLanguages;
+  }
 
-    @Selector()
-    static getTrendingRepositoriesLanguages(state: TrendingRepositoryStateModel): string[] {
-        return state.trendingRepositoriesLanguages;
-    }
+  @Selector()
+  static getCurrentLanguage(state: TrendingRepositoryStateModel): string {
+    return state.currentLanguage;
+  }
 
-    @Selector()
-    static getCurrentLanguage(state: TrendingRepositoryStateModel): string {
-        return state.currentLanguage;
-    }
+  @Selector()
+  static getLanguageTrendingRepositories(
+    state: TrendingRepositoryStateModel
+  ): Repository[] {
+    return state.languageTrendingRepositories;
+  }
 
-    @Selector()
-    static getLanguageTrendingRepositories(state: TrendingRepositoryStateModel): Repository[]{
-        return state.languageTrendingRepositories;
-    }
-
-    @Action(GetTrendingRepositoriesLanguages)
-    getLanguages({getState, setState}: StateContext<TrendingRepositoryStateModel>): Observable<string[]> {
-        return this.repositoryService.getTrendingRepositoriesLanguages().pipe(tap((result) => {
-            const state = getState();
-            setState({
-                ...state,
-                trendingRepositoriesLanguages: result,
-            });
-        }));
-    }
-
-    @Action(SetCurrentLanguage)
-    setCurrentLanguage({getState, setState}: StateContext<TrendingRepositoryStateModel>,
-                       { language }: SetCurrentLanguage): void {
+  @Action(GetTrendingRepositoriesLanguages)
+  getLanguages({
+    getState,
+    setState,
+  }: StateContext<TrendingRepositoryStateModel>): Observable<string[]> {
+    return this.repositoryService.getTrendingRepositoriesLanguages().pipe(
+      tap((result) => {
         const state = getState();
-        this.repositoryService.getLanguageTrendingRepositories(language)
-                                           .pipe(tap((result) => {
         setState({
+          ...state,
+          trendingRepositoriesLanguages: result,
+        });
+      })
+    );
+  }
+
+  @Action(SetCurrentLanguage)
+  setCurrentLanguage(
+    { getState, setState }: StateContext<TrendingRepositoryStateModel>,
+    { language }: SetCurrentLanguage
+  ): Observable<Repository[]> {
+    const state = getState();
+    return this.repositoryService
+      .getLanguageTrendingRepositories(language)
+      .pipe(
+        tap((result) => {
+          setState({
             ...state,
             currentLanguage: language,
             languageTrendingRepositories: result,
-        });
-    }));
-    }
-
+          });
+        })
+      );
+  }
 }
--- src/frontend/src/app/presentation/repositories/languages/languages.component.ts
+++ src/frontend/src/app/presentation/repositories/languages/languages.component.ts
@@ -2,27 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { GetTrendingRepositoriesLanguages, SetCurrentLanguage } from 'src/app/core/ngxs-state-management/actions/Trending.Repositories.actions';
+import {
+  GetTrendingRepositoriesLanguages,
+  SetCurrentLanguage,
+} from 'src/app/core/ngxs-state-management/actions/Trending.Repositories.actions';
 import { TrendingRepositoryState } from 'src/app/core/ngxs-state-management/states/Trending.Repositories.state';
 
 @Component({
   selector: 'app-languages',
   templateUrl: './languages.component.html',
-  styleUrls: ['./languages.component.scss']
+  styleUrls: ['./languages.component.scss'],
 })
 export class LanguagesComponent implements OnInit {
+  @Select(TrendingRepositoryState.getTrendingRepositoriesLanguages)
+  languages: Observable<string[]>;
 
-  @Select(TrendingRepositoryState.getTrendingRepositoriesLanguages) languages: Observable<string[]>;
-
-  constructor(private router: Router, private store: Store) { }
+  constructor(private router: Router, private store: Store) {}
 
   ngOnInit(): void {
     this.store.dispatch(new GetTrendingRepositoriesLanguages());
   }
 
-  getLanguagesRepositoriesInfos(language: string): void{
-    this.store.dispatch(new SetCurrentLanguage(language));
-    this.router.navigate(['repos']);
+  getLanguagesRepositoriesInfos(language: string): void {
+    console.log(language);
+    this.store.dispatch(new SetCurrentLanguage(language)).subscribe(
+      () => {
+        this.router.navigate(['repos']);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
